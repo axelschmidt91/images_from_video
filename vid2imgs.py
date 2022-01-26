ade5debeca53d2f27f8bedbbb389967d5c68e1e9
axelschmidt91 <axel.sebastian.schmidt@rwth-aachen.de>
2022-01-26 23:47:54 +0100

    first commit

---
+++ vid2imgs.py
@@ -0,0 +1,105 @@
+import cv2
+import logging
+import os
+
+
+def extraction(vidFile, numImages, outFolder):
+
+    cap = cv2.VideoCapture(vidFile)
+
+    """ get total frame count and saving interval for images"""
+    # framesPerSecond= int(cap.get(cv2.CAP_PROP_FPS))
+    frameCount = cap.cv2.CAP_PROP_FRAME_COUNT
+    savingInterval = int(frameCount / numImages)
+    logging.info(f"Saving interval is {savingInterval} frames")
+
+    success, image = cap.read()
+    count = 0
+
+    while success:
+        if not count % savingInterval:
+            imageName = f"frame{count:03}.jpg"
+            logging.info(f"Write image {imageName}")
+            cv2.imwrite(imageName, image)     # save frame as JPEG file      
+        success, image = cap.read()
+        logging.debug('Read a new frame: ', success)
+        count += 1
+
+
+if __name__ == "__main__":
+
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "file",
+        nargs="+",
+        help="folder to search for files")
+    parser.add_argument(
+        "-nI",
+        type=int,
+        nargs="?",
+        default=30,
+        dest="numImages",
+        help="Number of images the video should be subdivided into. Default=30.",
+    )
+    parser.add_argument(
+        "-del",
+        action="store_const",
+        const=True,
+        default=False,
+        dest="deletImages",
+        help="Delete previous images for selected video files before extract new ones. Useful if number of images has changed for the particular videos. Default=False",
+    )
+
+    levels = {
+        'critical': logging.CRITICAL,
+        'error': logging.ERROR,
+        'warning': logging.WARNING,
+        'info': logging.INFO,
+        'debug': logging.DEBUG
+    }
+    parser.add_argument(
+        '--log',
+        choices=levels.keys(),
+        default="info",
+        dest="log",
+        help=("Provide logging level. Default='info'.")
+    )
+
+    args = parser.parse_args()
+    files = args.files
+    numImages = args.numImages
+    deletImages = args.delImages
+
+    # Set up logging
+    if not os.path.isdir('log'):
+        os.mkdir('log')
+
+    logging.basicConfig(
+        level=levels[args.log],
+        format='%(asctime).19s [%(levelname)s]: %(message)s',
+        handlers=[
+            logging.FileHandler('log/vid2imgs.log'),
+            logging.StreamHandler()])
+
+    logging.info('Start vid2imgs logging')
+
+    for i, file in enumerate(files):
+
+        logging.info(f"progress: {i+1:3} / {len(files)}")
+
+        path, fileName = os.path.split(file)
+
+        imageFolderName = f"images_{fileName.split(".")[0]}"
+        imagePath = os.path.join(path, imageFolderName)
+
+        if not os.path.isdir(imagePath):
+            logging.info(f"Create Folder for images {imagePath}")
+            os.mkdir(imagePath)
+        elif deletImages:
+            logging.info(f"Delete images in folder {imagePath}")
+            for f in os.listdir(imagePath):
+                os.remove(os.path.join(imagePath, f))
+
+        extraction(file, numImages, imagePath)
